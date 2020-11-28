b84a40a45720653311b6966d6f8d5c24ad7c38b5
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-28 20:06:00 +0000

    Add global styling, theme provider & body.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled, {ThemeProvider} from 'styled-components'
+import {GlobalStyle, theme} from './theme'
+import {rem} from 'polished'
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalStyle>
+      <ThemeProvider theme={theme}>
+        <PageWrapper>
+        </PageWrapper>
+      </ThemeProvider>
+    </GlobalStyle>
   );
 }
 
-export default App;
+const PageWrapper = styled.body`
+  max-width: ${rem(1440)};
+`
