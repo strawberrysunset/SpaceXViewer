6d5f0041f339aaae32affc1b40d7770f85e0f5a1
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-28 20:01:22 +0000

    Add color shades.

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,8 +1,21 @@
+import {darken} from 'polished'
+
+// Original colors defined for tech test.
+const primary = "#215184"
+const dark = "#545454"
+const light = '#ffffff'
+
+// Generate additional darkened shades for hover/focus states etc.
 export const colors = {
     "primary": {
-        100: "#215184"
+        "100": primary,
+        "200": darken(0.8, primary),
+        "300": darken(0.6, primary)
     },
     "neutral": {
-        100: "#545454"
+        "100": dark,
+        "200": darken(0.8, dark),
+        "300": darken(0.6, dark),
+        "400": light
     }
 }
