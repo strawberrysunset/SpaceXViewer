d8daa0e4901f7f5968af931ba7e691a7dffa0b00
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-30 18:10:31 +0000

    Add return type.

--- src/utils/useFilteredArray.ts
+++ src/utils/useFilteredArray.ts
@@ -7,12 +7,17 @@ interface Props {
     filter?: Filter
 }
 
+interface ReturnType {
+    filteredArray: unknown[],
+    setFilter:  React.Dispatch<React.SetStateAction<Filter | undefined>>
+}
+
 export const filterArray = ({array, filter} : Props) => {
     if (!filter) return array
     return array.filter(filter)
 }
 
-export const useFilteredArray = ({array, filter: initialFilter} : Props) => {
+export const useFilteredArray = ({array, filter: initialFilter} : Props) : ReturnType => {
     const [filter, setFilter] = React.useState<Filter | undefined>(initialFilter)
     const filteredArray = filterArray({array, filter})
     return {filteredArray, setFilter}
