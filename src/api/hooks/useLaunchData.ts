7dc20aba4af5e71d864a901a6df2f935b7166ee8
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-30 18:36:34 +0000

    Remove hooks directory.

--- src/api/hooks/useLaunchData.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from 'react-query'
-import { getLaunchData } from '../getLaunchData'
-
-export const useLaunchData = () => {
-    return useQuery('launchData', getLaunchData, {
-        staleTime: Infinity, // Ensures query is never automatically re-fetched. 
-        keepPreviousData: true // Previous data will remain whilst query is being re-fetched.
-    })
-}
