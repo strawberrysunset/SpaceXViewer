9d8f1acbe17a63b8f042ecaf186f46a49feca9ed
StrawberrySunset <strawberrysunset@protonmail.com>
2020-11-30 18:09:39 +0000

    Add test and sampleAPI response files.

---
+++ src/api/sampleAPIResponse.ts
@@ -0,0 +1,369 @@
+// We have created a sample API response to prevent making API calls during testing.
+export const sampleAPIResponse = [
+    {
+        "fairings": {
+          "reused": false,
+          "recovery_attempt": false,
+          "recovered": false,
+          "ships": []
+        },
+        "links": {
+          "patch": {
+            "small": "https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png",
+            "large": "https://images2.imgbox.com/40/e3/GypSkayF_o.png"
+          },
+          "reddit": {
+            "campaign": null,
+            "launch": null,
+            "media": null,
+            "recovery": null
+          },
+          "flickr": {
+            "small": [],
+            "original": []
+          },
+          "presskit": null,
+          "webcast": "https://www.youtube.com/watch?v=0a_00nJ_Y88",
+          "youtube_id": "0a_00nJ_Y88",
+          "article": "https://www.space.com/2196-spacex-inaugural-falcon-1-rocket-lost-launch.html",
+          "wikipedia": "https://en.wikipedia.org/wiki/DemoSat"
+        },
+        "static_fire_date_utc": "2006-03-17T00:00:00.000Z",
+        "static_fire_date_unix": 1142553600,
+        "tbd": false,
+        "net": false,
+        "window": 0,
+        "rocket": "5e9d0d95eda69955f709d1eb",
+        "success": false,
+        "details": "Engine failure at 33 seconds and loss of vehicle",
+        "crew": [],
+        "ships": [],
+        "capsules": [],
+        "payloads": [
+          "5eb0e4b5b6c3bb0006eeb1e1"
+        ],
+        "launchpad": "5e9e4502f5090995de566f86",
+        "auto_update": true,
+        "failures": [
+          {
+            "time": 33,
+            "altitude": null,
+            "reason": "merlin engine failure"
+          }
+        ],
+        "flight_number": 1,
+        "name": "FalconSat",
+        "date_utc": "2006-03-24T22:30:00.000Z",
+        "date_unix": 1143239400,
+        "date_local": "2006-03-25T10:30:00+12:00",
+        "date_precision": "hour",
+        "upcoming": false,
+        "cores": [
+          {
+            "core": "5e9e289df35918033d3b2623",
+            "flight": 1,
+            "gridfins": false,
+            "legs": false,
+            "reused": false,
+            "landing_attempt": false,
+            "landing_success": null,
+            "landing_type": null,
+            "landpad": null
+          }
+        ],
+        "id": "5eb87cd9ffd86e000604b32a"
+      },
+      {
+        "fairings": {
+            "reused": false,
+            "recovery_attempt": false,
+            "recovered": false,
+            "ships": []
+        },
+        "links": {
+            "patch": {
+                "small": "https://images2.imgbox.com/4f/e3/I0lkuJ2e_o.png",
+                "large": "https://images2.imgbox.com/be/e7/iNqsqVYM_o.png"
+            },
+            "reddit": {
+                "campaign": null,
+                "launch": null,
+                "media": null,
+                "recovery": null
+            },
+            "flickr": {
+                "small": [],
+                "original": []
+            },
+            "presskit": null,
+            "webcast": "https://www.youtube.com/watch?v=Lk4zQ2wP-Nc",
+            "youtube_id": "Lk4zQ2wP-Nc",
+            "article": "https://www.space.com/3590-spacex-falcon-1-rocket-fails-reach-orbit.html",
+            "wikipedia": "https://en.wikipedia.org/wiki/DemoSat"
+        },
+        "static_fire_date_utc": null,
+        "static_fire_date_unix": null,
+        "tbd": false,
+        "net": false,
+        "window": 0,
+        "rocket": "5e9d0d95eda69955f709d1eb",
+        "success": false,
+        "details": "Successful first stage burn and transition to second stage, maximum altitude 289 km, Premature engine shutdown at T+7 min 30 s, Failed to reach orbit, Failed to recover first stage",
+        "crew": [],
+        "ships": [],
+        "capsules": [],
+        "payloads": [
+            "5eb0e4b6b6c3bb0006eeb1e2"
+        ],
+        "launchpad": "5e9e4502f5090995de566f86",
+        "auto_update": true,
+        "failures": [
+            {
+                "time": 301,
+                "altitude": 289,
+                "reason": "harmonic oscillation leading to premature engine shutdown"
+            }
+        ],
+        "flight_number": 2,
+        "name": "DemoSat",
+        "date_utc": "2007-03-21T01:10:00.000Z",
+        "date_unix": 1174439400,
+        "date_local": "2007-03-21T13:10:00+12:00",
+        "date_precision": "hour",
+        "upcoming": false,
+        "cores": [
+            {
+                "core": "5e9e289ef35918416a3b2624",
+                "flight": 1,
+                "gridfins": false,
+                "legs": false,
+                "reused": false,
+                "landing_attempt": false,
+                "landing_success": null,
+                "landing_type": null,
+                "landpad": null
+            }
+        ],
+        "id": "5eb87cdaffd86e000604b32b"
+    },
+    {
+        "fairings": {
+            "reused": false,
+            "recovery_attempt": false,
+            "recovered": false,
+            "ships": []
+        },
+        "links": {
+            "patch": {
+                "small": "https://images2.imgbox.com/3d/86/cnu0pan8_o.png",
+                "large": "https://images2.imgbox.com/4b/bd/d8UxLh4q_o.png"
+            },
+            "reddit": {
+                "campaign": null,
+                "launch": null,
+                "media": null,
+                "recovery": null
+            },
+            "flickr": {
+                "small": [],
+                "original": []
+            },
+            "presskit": null,
+            "webcast": "https://www.youtube.com/watch?v=v0w9p3U8860",
+            "youtube_id": "v0w9p3U8860",
+            "article": "http://www.spacex.com/news/2013/02/11/falcon-1-flight-3-mission-summary",
+            "wikipedia": "https://en.wikipedia.org/wiki/Trailblazer_(satellite)"
+        },
+        "static_fire_date_utc": null,
+        "static_fire_date_unix": null,
+        "tbd": false,
+        "net": false,
+        "window": 0,
+        "rocket": "5e9d0d95eda69955f709d1eb",
+        "success": false,
+        "details": "Residual stage 1 thrust led to collision between stage 1 and stage 2",
+        "crew": [],
+        "ships": [],
+        "capsules": [],
+        "payloads": [
+            "5eb0e4b6b6c3bb0006eeb1e3",
+            "5eb0e4b6b6c3bb0006eeb1e4"
+        ],
+        "launchpad": "5e9e4502f5090995de566f86",
+        "auto_update": true,
+        "failures": [
+            {
+                "time": 140,
+                "altitude": 35,
+                "reason": "residual stage-1 thrust led to collision between stage 1 and stage 2"
+            }
+        ],
+        "flight_number": 3,
+        "name": "Trailblazer",
+        "date_utc": "2008-08-03T03:34:00.000Z",
+        "date_unix": 1217734440,
+        "date_local": "2008-08-03T15:34:00+12:00",
+        "date_precision": "hour",
+        "upcoming": false,
+        "cores": [
+            {
+                "core": "5e9e289ef3591814873b2625",
+                "flight": 1,
+                "gridfins": false,
+                "legs": false,
+                "reused": false,
+                "landing_attempt": false,
+                "landing_success": null,
+                "landing_type": null,
+                "landpad": null
+            }
+        ],
+        "id": "5eb87cdbffd86e000604b32c"
+    },
+    {
+        "fairings": null,
+        "links": {
+            "patch": {
+                "small": "https://images2.imgbox.com/53/22/dh0XSLXO_o.png",
+                "large": "https://images2.imgbox.com/15/2b/NAcsTEB6_o.png"
+            },
+            "reddit": {
+                "campaign": "https://www.reddit.com/r/spacex/comments/ezn6n0/crs20_launch_campaign_thread",
+                "launch": "https://www.reddit.com/r/spacex/comments/fe8pcj/rspacex_crs20_official_launch_discussion_updates/",
+                "media": "https://www.reddit.com/r/spacex/comments/fes64p/rspacex_crs20_media_thread_videos_images_gifs/",
+                "recovery": null
+            },
+            "flickr": {
+                "small": [],
+                "original": [
+                    "https://live.staticflickr.com/65535/49635401403_96f9c322dc_o.jpg",
+                    "https://live.staticflickr.com/65535/49636202657_e81210a3ca_o.jpg",
+                    "https://live.staticflickr.com/65535/49636202572_8831c5a917_o.jpg",
+                    "https://live.staticflickr.com/65535/49635401423_e0bef3e82f_o.jpg",
+                    "https://live.staticflickr.com/65535/49635985086_660be7062f_o.jpg"
+                ]
+            },
+            "presskit": "https://www.spacex.com/sites/spacex/files/crs-20_mission_press_kit.pdf",
+            "webcast": "https://youtu.be/1MkcWK2PnsU",
+            "youtube_id": "1MkcWK2PnsU",
+            "article": "https://spaceflightnow.com/2020/03/07/late-night-launch-of-spacex-cargo-ship-marks-end-of-an-era/",
+            "wikipedia": "https://en.wikipedia.org/wiki/SpaceX_CRS-20"
+        },
+        "static_fire_date_utc": "2020-03-01T10:20:00.000Z",
+        "static_fire_date_unix": 1583058000,
+        "tbd": false,
+        "net": false,
+        "window": 0,
+        "rocket": "5e9d0d95eda69973a809d1ec",
+        "success": true,
+        "details": "SpaceX's 20th and final Crew Resupply Mission under the original NASA CRS contract, this mission brings essential supplies to the International Space Station using SpaceX's reusable Dragon spacecraft. It is the last scheduled flight of a Dragon 1 capsule. (CRS-21 and up under the new Commercial Resupply Services 2 contract will use Dragon 2.) The external payload for this mission is the Bartolomeo ISS external payload hosting platform. Falcon 9 and Dragon will launch from SLC-40, Cape Canaveral Air Force Station and the booster will land at LZ-1. The mission will be complete with return and recovery of the Dragon capsule and down cargo.",
+        "crew": [],
+        "ships": [],
+        "capsules": [
+            "5e9e2c5cf359185d753b266f"
+        ],
+        "payloads": [
+            "5eb0e4d0b6c3bb0006eeb253"
+        ],
+        "launchpad": "5e9e4501f509094ba4566f84",
+        "auto_update": true,
+        "failures": [],
+        "flight_number": 91,
+        "name": "CRS-20",
+        "date_utc": "2020-03-07T04:50:31.000Z",
+        "date_unix": 1583556631,
+        "date_local": "2020-03-06T23:50:31-05:00",
+        "date_precision": "hour",
+        "upcoming": false,
+        "cores": [
+            {
+                "core": "5e9e28a7f359187afd3b2662",
+                "flight": 2,
+                "gridfins": true,
+                "legs": true,
+                "reused": true,
+                "landing_attempt": true,
+                "landing_success": true,
+                "landing_type": "RTLS",
+                "landpad": "5e9e3032383ecb267a34e7c7"
+            }
+        ],
+        "id": "5eb87d42ffd86e000604b384"
+    },
+    {
+        "fairings": {
+            "reused": true,
+            "recovery_attempt": true,
+            "recovered": null,
+            "ships": [
+                "5ea6ed2e080df4000697c908",
+                "5ea6ed2e080df4000697c907"
+            ]
+        },
+        "links": {
+            "patch": {
+                "small": "https://images2.imgbox.com/9a/96/nLppz9HW_o.png",
+                "large": "https://images2.imgbox.com/d2/3b/bQaWiil0_o.png"
+            },
+            "reddit": {
+                "campaign": "https://www.reddit.com/r/spacex/comments/gwbr4t/starlink8_launch_campaign_thread/",
+                "launch": "https://www.reddit.com/r/spacex/comments/h7gqlc/rspacex_starlink_8_official_launch_discussion/",
+                "media": "https://www.reddit.com/r/spacex/comments/h842qk/rspacex_starlink8_media_thread_photographer/",
+                "recovery": "https://www.reddit.com/r/spacex/comments/h8sx6q/starlink8_recovery_thread/"
+            },
+            "flickr": {
+                "small": [],
+                "original": [
+                    "https://live.staticflickr.com/65535/50009748327_93e52a451f_o.jpg"
+                ]
+            },
+            "presskit": null,
+            "webcast": "https://youtu.be/8riKQXChPGg",
+            "youtube_id": "8riKQXChPGg",
+            "article": "https://spaceflightnow.com/2020/06/13/starlink-satellite-deployments-continue-with-successful-falcon-9-launch/",
+            "wikipedia": "https://en.wikipedia.org/wiki/Starlink"
+        },
+        "static_fire_date_utc": null,
+        "static_fire_date_unix": null,
+        "tbd": false,
+        "net": false,
+        "window": 0,
+        "rocket": "5e9d0d95eda69973a809d1ec",
+        "success": true,
+        "details": "This mission will launch the eighth batch of operational Starlink satellites, which are expected to be version 1.0, from SLC-40, Cape Canaveral AFS. It is the ninth Starlink launch overall. The satellites will be delivered to low Earth orbit and will spend a few weeks maneuvering to their operational altitude of 550 km. This mission is includes rideshare payloads, SkySats 16-18, on top of the Starlink stack. The booster for this mission is expected to land an ASDS.",
+        "crew": [],
+        "ships": [
+            "5ea6ed2e080df4000697c908",
+            "5ea6ed2e080df4000697c907",
+            "5ea6ed2f080df4000697c90b"
+        ],
+        "capsules": [],
+        "payloads": [
+            "5eb0e4d1b6c3bb0006eeb258"
+        ],
+        "launchpad": "5e9e4501f509094ba4566f84",
+        "auto_update": true,
+        "failures": [],
+        "flight_number": 96,
+        "name": "Starlink-8 & SkySat 16-18",
+        "date_utc": "2020-06-13T09:21:00.000Z",
+        "date_unix": 1592040060,
+        "date_local": "2020-06-13T05:21:00-04:00",
+        "date_precision": "hour",
+        "upcoming": false,
+        "cores": [
+            {
+                "core": "5e9e28a7f359187afd3b2662",
+                "flight": 3,
+                "gridfins": true,
+                "legs": true,
+                "reused": true,
+                "landing_attempt": true,
+                "landing_success": true,
+                "landing_type": "ASDS",
+                "landpad": "5e9e3032383ecb6bb234e7ca"
+            }
+        ],
+        "id": "5eb87d46ffd86e000604b389"
+    }
+]
