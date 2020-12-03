ddd218a44579aac3d58a5d0dfa48ebb062299679
StrawberrySunset <strawberrysunset@protonmail.com>
2020-12-03 15:34:45 +0000

    Change prop types, card props & update styles.

--- src/components/LaunchCardList.tsx
+++ src/components/LaunchCardList.tsx
@@ -1,34 +1,35 @@
+import { rem } from 'polished'
 import React from 'react'
 import styled, {css} from 'styled-components'
+import moment from 'moment'
 import { LaunchCard } from './LaunchCard'
 
 interface Props {
-    launchData: [],
+    launches: any,
     isFetching?: boolean,
     isError?: boolean,
-    errorMsg?: string,
+    error?: any
 }
 
-export const LaunchCardList = ({launchData = [], isFetching, isError, errorMsg} : Props) => {
+export const LaunchCardList = ({launches, isFetching, isError, error} : Props) => {
 
-    if (isError) return <Message error={true}>{errorMsg}</Message>
+    if (isError) return <Message error={true}>{error?.message}</Message>
     if (isFetching) return <Message>Fetching Data...</Message>
 
     return (
         <ListWrapper>
-            {launchData.map((launch : any, idx : number) => (
+            {launches.map((launch : any) => (
                 <LaunchCard 
-                    as="li"
-                    number={idx}
-                    vehicle={launch.vehicle}
-                    date={launch.date}
-                    mission={launch.mission}
+                    key={launch.id}
+                    number={launch.flightNumber}
+                    name={launch.name}
+                    rocket={launch.rocket}
+                    date={moment(launch.date).format("Do MMM YYYY")}
                 />
             ))}
         </ListWrapper>
     )
 }
-
 
 const Message = styled.p<{ error?: boolean }>`
     width: 100%;
@@ -37,8 +38,11 @@ const Message = styled.p<{ error?: boolean }>`
     ${props => props.error && css`
         color: ${props.theme.colors.primary[100]};
     `}
+    margin-top: ${rem(48)};
 `
 
 const ListWrapper = styled.ol`
     list-style: none;
+    display: grid;
+    grid-gap: ${rem(8)};
 `
