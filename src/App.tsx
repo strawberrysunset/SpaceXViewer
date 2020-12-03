428015b7fcaac9f5c7dde60a8a6df588ffe2170d
StrawberrySunset <strawberrysunset@protonmail.com>
2020-12-03 01:36:51 +0000

    Import components and update wrappers..

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,40 @@ import styled, {ThemeProvider} from 'styled-components'
 import {GlobalStyle, theme} from './theme'
 import {QueryCache, ReactQueryCacheProvider} from 'react-query'
 import {rem} from 'polished'
+import {Header, HeroImage, LaunchTable} from './components';
 
+// React query requires global provider for caching server state.
 const queryCache = new QueryCache()
 
 export const App = () => {
   return (
-    <GlobalStyle>
+    <React.Fragment>
+      <GlobalStyle/>
       <ThemeProvider theme={theme}>
         <ReactQueryCacheProvider queryCache={queryCache}>
-          <PageWrapper>
-          </PageWrapper>
+           <PageWrapper>
+            <Header />
+            <MainWrapper>
+              <HeroImage/>
+              <LaunchTable/>
+            </MainWrapper>
+          </PageWrapper> 
         </ReactQueryCacheProvider>
       </ThemeProvider>
-    </GlobalStyle>
+    </React.Fragment>
   );
 }
 
-const PageWrapper = styled.body`
+const PageWrapper = styled.div`
   max-width: ${rem(1440)};
+  padding-left: ${rem(40.92)};
+  padding-top: ${rem(32.7)};
+  margin: 0 auto;
+`
+
+const MainWrapper = styled.main`
+  margin: ${rem(52)} auto;
+  padding: 0 ${rem(80)};
+  display: flex;
+  align-items: flex-start;
 `
