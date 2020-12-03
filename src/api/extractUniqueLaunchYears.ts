42693a11e7dde1ec1c7b9c4b0cd21ccbd02fa797
StrawberrySunset <strawberrysunset@protonmail.com>
2020-12-03 04:06:03 +0000

    Add return type.

--- src/api/extractUniqueLaunchYears.ts
+++ src/api/extractUniqueLaunchYears.ts
@@ -5,7 +5,7 @@ interface Props {
 }
 
 // Get deduped array of launch years.
-export const extractUniqueLaunchYears = ({launches} : Props) => {
+export const extractUniqueLaunchYears = ({launches} : Props) : number[] => {
     return launches.reduce((years: number[], launch) : number[]  => {
         const year = launch.date.getFullYear()
         return (years.indexOf(year) === -1) ? [...years, year] : years 
