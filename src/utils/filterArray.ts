3215e7d78a5377476e5f9d11922b59fbd9250f79
StrawberrySunset <strawberrysunset@protonmail.com>
2020-12-03 04:04:14 +0000

    Remove undefined type value.

--- src/utils/filterArray.ts
+++ src/utils/filterArray.ts
@@ -1,11 +1,10 @@
 export type Filter<T> = (state: T) => boolean 
 
 export interface Props<T> {
-    array: T[],
-    filter?: Filter<T>
+    array: T[] | undefined,
+    filter?: Filter<T> | undefined
 }
 
-export function filterArray<T> ({array, filter} : Props<T>) {
-    if (!filter) return array
-    return array.filter(filter)
+export function filterArray<T> ({array = [], filter} : Props<T>) : T[] {
+    return filter ? array.filter(filter) : array
 }
--- src/utils/sortArray.ts
+++ src/utils/sortArray.ts
@@ -1,5 +1,5 @@
 export interface Props<T> {
-    array: T[] // Array that will be sorted.
+    array: T[] | undefined // Array that will be sorted.
     direction: 'ascending' | 'descending', // Sorting direction.
     statePick(value: T) : number // Function for picking state which will be compared.
 }
