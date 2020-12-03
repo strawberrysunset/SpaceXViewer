7d9ed8ac9cac8b73fe677e6820b14e34efb319aa
StrawberrySunset <strawberrysunset@protonmail.com>
2020-12-03 01:38:48 +0000

    Add styles, wrapper markup & change props.

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,36 +1,62 @@
 import React from 'react'
-import styled, { AnyStyledComponent } from 'styled-components'
-import {rem} from 'polished'
+import styled from 'styled-components'
+import {em} from 'polished'
 
 interface Props {
-    children: React.ReactChildren,
-    icon?: AnyStyledComponent,
+    children?: any,
+    icon?: () => JSX.Element,
 }
 
 export const Button = ({ children, icon: Icon, ...rest } : Props) => {
     return (
-        <Wrapper {...rest}>
-            <Text>{children}</Text>
-            {Icon && <Icon css={`width: ${rem(16)}; height: ${rem(16)};`}/>}
+        <Wrapper>
+            {Icon && (
+                <IconWrapper>
+                    <Icon/>
+                </IconWrapper>
+            )}
+            <ButtonWrapper {...rest}>
+                {children}
+            </ButtonWrapper>
         </Wrapper>
     )
 }
 
+const Wrapper = styled.div`
+    position: relative;
+`
 
-const Wrapper = styled.button`
+const IconWrapper = styled.div`
+    position: absolute;
+    right: 0;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    margin-right: ${em(12)};
+    pointer-events: none; // Allow click through to button;
+`
+
+const ButtonWrapper = styled.button`
     border: none;
     text-decoration: none;
-    display: flex;
-    justify-content: space-between;
-    background: ${props => props.theme.colors.primary};
+    appearance: none;
+    color: ${props => props.theme.colors.neutral[300]};
+    background: ${props => props.theme.colors.primary[100]}; 
+    padding: ${em(10)} ${em(16)};
+    padding-right: ${em(36)};
+    white-space: nowrap;
+    font-weight: 700;
     :hover {
+        cursor: pointer;
         background: ${props => props.theme.colors.primary[200]};
     }
-    :focus {
+    :active {
         background: ${props => props.theme.colors.primary[300]};
+    }
+    :disabled {
+        background: ${props => props.theme.colors.neutral[200]};
+        cursor: not-allowed;
     }
 `
 
-const Text = styled.span`
-    color: ${props => props.theme.colors.neutral[400]};
-`
+
