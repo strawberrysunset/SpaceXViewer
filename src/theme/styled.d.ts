5ffbc3a26168a4da317cdd8b5719f930e85a1f64
StrawberrySunset <strawberrysunset@protonmail.com>
2020-12-03 00:53:06 +0000

    Add styled components prop attributes.

--- src/theme/styled.d.ts
+++ src/theme/styled.d.ts
@@ -1,4 +1,4 @@
-import {} from 'styled-components';
+import { CSSProp } from 'styled-components';
 import theme from './index';
 
 // Get type from user defined theme and set it as the default styled-components theme interface.
@@ -6,3 +6,12 @@ declare module 'styled-components' {
   type Theme = typeof theme;
   export interface DefaultTheme extends Theme {}
 }
+
+declare module 'react' {
+  interface Attributes {
+    css?: CSSProp | CSSObject,
+    as?: string,
+    [x: string]: any 
+  }
+}
+
