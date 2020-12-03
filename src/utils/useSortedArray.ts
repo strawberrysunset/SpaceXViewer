d516f87fe5ac900759ea832a17dd1822733a7a27
StrawberrySunset <strawberrysunset@protonmail.com>
2020-12-03 01:17:05 +0000

    Extract sorting function & add hook dep.

--- src/utils/useSortedArray.ts
+++ src/utils/useSortedArray.ts
@@ -1,34 +1,20 @@
 // Sort an array in ascending or descending order.
 import React from 'react'
+import {sortArray, Props} from './sortArray'
 
-interface Props {
-    array: unknown[] // Array that will be sorted.
-    direction: 'ascending' | 'descending', // Sorting direction.
-    statePick(value: unknown) : unknown // Function for picking state which will be compared.
-}
-
-interface Return {
-    sortedArray: any[],
+interface ReturnType<T> {
+    sortedArray: T[],
     toggleSortingDirection: () => void
 }
 
-export const sortArray = ({array, direction, statePick} : Props) => {
-    return array.sort((item1, item2) => {
-        const stateA = statePick(item1)
-        const stateB = statePick(item2)
-        if (direction === 'ascending') return (stateA - stateB)
-        if (direction === 'descending') return (stateB - stateA)
-    })
-}
-
-export const useSortedArray = ({ array, direction: initialDirection, statePick } : Props) : Return => {
+export function useSortedArray<T>({ array = [], direction: initialDirection, statePick } : Props<T>) : ReturnType<T> {
     
     const [direction, setDirection] = React.useState(initialDirection)
     const sortedArray = sortArray({array, direction, statePick});
     
     const toggleSortingDirection = React.useCallback(() => {
         setDirection((direction === 'ascending') ? 'descending' : 'ascending')
-    }, [setDirection])
+    }, [setDirection, direction])
     
     return {sortedArray, toggleSortingDirection}
 }
