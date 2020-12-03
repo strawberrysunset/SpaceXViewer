db669a07635de2389c1f74c835a80c41c7e37848
StrawberrySunset <strawberrysunset@protonmail.com>
2020-12-03 01:43:24 +0000

    Align items center.

--- src/components/graphics/images/SiteLogo.tsx
+++ src/components/graphics/images/SiteLogo.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-import SpaceXLogo from '../assets/spacex-logo.png'
+import SpaceXLogo from '../../../assets/spacex-logo.png'
 import {rem} from 'polished'
 
 export const SiteLogo = () => {
@@ -14,14 +14,18 @@ export const SiteLogo = () => {
 
 const Wrapper = styled.div`
     display: flex;
-    align-items: center;
+    align-items: flex-start;
 `
 
 const Logo = styled.img`
     width: ${rem(178.75)};
     height: ${rem(22)};
+    user-select: none;
 `
 
-const Text = styled.span`
+const Text = styled.div`
+    font-size: ${props => props.theme.typeScale[200]};
     text-transform: uppercase;
+    margin-top: ${rem('4px')};
+    margin-left: ${rem(-12)};
 `
