6b45992c4a6c62bb2e83cd682d4ce0d22faa04c9
StrawberrySunset <strawberrysunset@protonmail.com>
2020-12-03 17:20:39 +0000

    Add comment.

--- src/utils/useFilteredArray.ts
+++ src/utils/useFilteredArray.ts
@@ -7,6 +7,7 @@ interface ReturnType<T> {
     clearFilter: () => void
 }
 
+// useState hook treats function values as lazy initializer. Therefore, to store a function we must nest it within another function.
 export function useFilteredArray<T> ({array = [], filter: initialFilter} : Props<T>) : ReturnType<T> {
     const [filter, setFilter] = React.useState<Filter<T> | undefined>(() => initialFilter)
     const filteredArray = filterArray<T>({array, filter})
