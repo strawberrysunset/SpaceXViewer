e888fabe35d0ecea5629ab97ab3dd50a5fbf91aa
StrawberrySunset <strawberrysunset@protonmail.com>
2020-12-03 01:48:47 +0000

    Set window focus refetch to false.

--- src/api/useLaunchData.ts
+++ src/api/useLaunchData.ts
@@ -4,6 +4,7 @@ import { getLaunchData } from './getLaunchData'
 export const useLaunchData = () => {
     return useQuery('launchData', getLaunchData, {
         staleTime: Infinity, // Ensures query is never automatically re-fetched. 
-        keepPreviousData: true // Previous data will remain whilst query is being re-fetched.
+        keepPreviousData: true, // Previous data will remain whilst query is being re-fetched.
+        refetchOnWindowFocus: false
     })
 }
