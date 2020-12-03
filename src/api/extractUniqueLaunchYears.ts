a2b95547f80d039291d7addf39b96ab758331857
StrawberrySunset <strawberrysunset@protonmail.com>
2020-12-03 01:40:36 +0000

    Add launch year extraction function.

---
+++ src/api/extractUniqueLaunchYears.ts
@@ -0,0 +1,13 @@
+import {LaunchData} from './types'
+
+interface Props {
+    launches: LaunchData[]
+}
+
+// Get deduped array of launch years.
+export const extractUniqueLaunchYears = ({launches} : Props) => {
+    return launches.reduce((years: number[], launch) : number[]  => {
+        const year = launch.date.getFullYear()
+        return (years.indexOf(year) === -1) ? [...years, year] : years 
+    }, [])
+}
